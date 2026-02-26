a92f2dbb7639e448b29a8c07ed965a516daee7d9
Sumit Thakur <sumit@thexyzstudio.com>
2026-02-26 14:08:28 +0530

    Add SVG assets for dashboard, earnings, payouts, and users with fill variants

--- src/assets/exports.ts
+++ src/assets/exports.ts
@@ -5,6 +5,25 @@ import DummyPROFILE from "./dummyProfile.webp";
 import Setting from "./setting.svg";
 import Notification from "./notification.svg";
 import Search from "./search.svg";
+import Logout from "./logout.svg";
+
+// Route Icons
+import App from "./app.svg";
+import CheckIn from "./checkIn.svg";
+import ClubReq from "./clubRequest.svg";
+import Clubs from "./clubs.svg";
+import Dashboard from "./dashboard.svg";
+import Earn from "./earnings.svg";
+import Payouts from "./payouts.svg";
+import User from "./users.svg";
+import AppFill from "./appFill.svg";
+import CheckInFill from "./checkInFill.svg";
+import ClubReqFill from "./clubRequestFill.svg";
+import ClubsFill from "./clubsFill.svg";
+import DashboardFill from "./dashboardFill.svg";
+import EarnFill from "./earningsFill.svg";
+import PayoutsFill from "./payoutsFill.svg";
+import UserFill from "./usersFill.svg";
 
 export const ICONS = {
   FITFOB,
@@ -13,6 +32,10 @@ export const ICONS = {
   Setting,
   Notification,
   Search,
+  Logout,
+  // Route Icons
+  App, CheckIn, ClubReq, Clubs, Dashboard, Earn, Payouts, User,
+  AppFill, CheckInFill, ClubReqFill, ClubsFill, DashboardFill, EarnFill, PayoutsFill, UserFill,
 };
 
 export const IMAGES = {
